c737cfd329f8aff2676ddb3626118e56344072fb
Malti1 <maltichumber123@gmail.com>
2022-01-14 17:52:57 +0000

    Home Work

---
+++ ZeroCondition.java
@@ -0,0 +1,17 @@
+package operators;
+
+public class ZeroCondition {
+
+    public static void main(String[] args) {
+
+   int x = 0;
+   if (x>0){
+       System.out.println("Number will be positive");//positive
+   } else if (x==0){
+       System.out.println("Number will be positive"); //positive
+   } else {
+            System.out.println("Number will be negative"); //negative
+        }
+    }}
+
+
